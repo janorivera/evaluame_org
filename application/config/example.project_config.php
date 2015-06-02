07cdc989c603d13b77c85cd5eee0644088ea4afb
janorivera <janorivera@github.com>
2015-06-02 17:02:54 +0800

    Adding example files: database config and project config files.

---
+++ application/config/example.project_config.php
@@ -0,0 +1,19 @@
+<?php
+//fill this section with your facebook and twitter details
+
+$config['facebook']['api_id'] = '**********';
+$config['facebook']['app_secret'] = '**********';
+$config['facebook']['redirect_url'] = base_url();
+$config['facebook']['permissions'] = array(
+		'email',
+		'user_location',
+		'user_birthday'
+);
+
+
+$config['twitter']['settings']= array(
+				'oauth_access_token' => "*******",
+				'oauth_access_token_secret' => "********",
+				'consumer_key' => "*********",
+				'consumer_secret' => "**********"
+		);
---
+++ application/config/example.project_constants.php
@@ -0,0 +1,4 @@
+<?php
+define('URL_TWEETS', 'https://api.twitter.com/1.1/search/tweets.json');
+define('URL_USER_TWEETS', 'https://api.twitter.com/1.1/statuses/user_timeline.json');
+//Define here project constants
