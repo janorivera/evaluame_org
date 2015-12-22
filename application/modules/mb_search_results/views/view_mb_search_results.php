529a380f5afb69e83f858b948c2fc0d7be8c27c1
janorivera <janorivera@github.com>
2015-12-22 10:48:30 +0800

    search feature

--- application/modules/mb_search_results/views/view_mb_search_results.php
+++ application/modules/mb_search_results/views/view_mb_search_results.php
@@ -1,19 +1,41 @@
 
 <div id="page-wrapper" style="min-height: 489px;">
 
-<p> test </p>
-<p>
-aqui iria
-<?php
-foreach ($searchResults as $result)
-{
-	echo $result['FirstName'];
-	echo $result['LastName'];
-	echo $result['PartyName'];
-}
-echo 'test echo';
+	<h4>Resultados de la búsqueda</h4>
 
-?>
-</p>
+	<div class="news news-panel">
+		<ul>
+								<?php
+								if (empty( $searchResults )){
+									
+									echo '<p>No se encontraron resultados para tu búsqueda ...</p>';
+									
+								}
+								
+								foreach ( $searchResults as $result ) {
+									echo '<li>';
+									// echo '<img src="' . $unitNews ['image'] . '" />';
+									echo '<img
+											src="' . get_media_path ( 'images/politician_pictures', $result ['PoliticianPicturePath'], 'jpg' ) . '"
+													alt="" style="width: 100px; height: 100px; padding: 10px">';
+									
+									echo '<dl>';
+									
+									echo '<a href="' . base_url ( 'mb_politician_info/render_template' ) . '/' . $result ['PoliticianId'] . '" >';
+									echo '<dt class= "text-info"><strong>' . $result ['FirstName'] . ' ' . $result ['LastName'] . '</strong></dt>';
+									echo '</a>';
+									echo '<dd><strong>' . $result ['TypeName'] . '</strong></dd>';
+									echo '<dd>' . $result ['PartyName'] . '</dd>';
+									echo '<dd>' . $result ['RegionName'] . '</dd>';
+									echo '</dl>';
+									
+									echo '</li>';
+								}
+								?>
+
+								</ul>
+	</div>
+
+	</p>
 
 </div>
